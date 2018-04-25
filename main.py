944bd4c950c1cc921d8347b8373a7bd5cc3f3c48
wrijus <wrijus1@yahoo.com>
2018-04-24 22:34:21 -0500

    Frist commit for blogz OOW

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog2018:makeit2sec3@localhost:8889/build-a-blog2018'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:allmostth3r3@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+
 
 class Blog(db.Model):
 
@@ -17,6 +18,26 @@ class Blog(db.Model):
         self.title = title
         self.body = body
 
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 
 @app.route('/blog', methods=['POST', 'GET'])
